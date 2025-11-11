39a908918f5cfb6f9fa66f7776c5a6a853884957
nguyendieu245 <nguyendieuonce@gmail.com>
2025-11-11 22:40:05 +0700

    models/Post.php

--- models/Post.php
+++ models/Post.php
@@ -9,10 +9,11 @@ class Post
         $this->conn = $db;
     }
 
+    // Lấy danh sách bài viết
     public function all()
     {
         $stmt = $this->conn->query("
-            SELECT p.*, c.name AS category_name 
+            SELECT p.*, c.name AS category_name
             FROM {$this->table} p
             LEFT JOIN categories c ON p.category_id = c.id
             ORDER BY p.created_at DESC
@@ -20,18 +21,20 @@ class Post
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    // Lấy 1 bài viết cụ thể
     public function find($id)
     {
         $stmt = $this->conn->prepare("
-            SELECT p.*, c.name AS category_name 
+            SELECT p.*, c.name AS category_name
             FROM {$this->table} p
             LEFT JOIN categories c ON p.category_id = c.id
-            WHERE p.id=?
+            WHERE p.id = ?
         ");
         $stmt->execute([$id]);
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
+    // Tạo bài viết mới
     public function create($title, $content, $author, $image, $status, $category_id)
     {
         $stmt = $this->conn->prepare("
@@ -41,16 +44,18 @@ class Post
         return $stmt->execute([$title, $content, $author, $image, $status, $category_id]);
     }
 
+    // Cập nhật bài viết
     public function update($id, $title, $content, $image, $status, $category_id)
     {
         $stmt = $this->conn->prepare("
-            UPDATE {$this->table} 
-            SET title=?, content=?, image=?, status=?, category_id=? 
+            UPDATE {$this->table}
+            SET title=?, content=?, image=?, status=?, category_id=?
             WHERE id=?
         ");
         return $stmt->execute([$title, $content, $image, $status, $category_id, $id]);
     }
 
+    // Xóa bài viết
     public function delete($id)
     {
         $stmt = $this->conn->prepare("DELETE FROM {$this->table} WHERE id=?");
