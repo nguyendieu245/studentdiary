4ab3e910e681b92eaa5b01a37f5e60a4d4139770
nguyendieu245 <nguyendieuonce@gmail.com>
2025-11-18 22:44:39 +0700

    sua

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -13,32 +13,6 @@ class UserController {
         include __DIR__ . '/../views/user/list.php';
     }
 
-    public function create() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $username = $_POST['username'];
-            $password = $_POST['password'];
-            $fullname = $_POST['fullname'];
-            $email = $_POST['email'];
-            $role = $_POST['role'] ?? 'user';
-            $this->user->create($username, $password, $fullname, $email, $role);
-            header('Location: index.php?action=users');
-            exit;
-        }
-        include __DIR__ . '/../views/user/form.php';
-    }
-
-    public function edit($id) {
-        $user = $this->user->find($id);
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $fullname = $_POST['fullname'];
-            $email = $_POST['email'];
-            $this->user->update($id, $fullname, $email);
-            header('Location: index.php?action=users');
-            exit;
-        }
-        include __DIR__ . '/../views/user/form.php';
-    }
-
     public function delete($id) {
         $this->user->delete($id);
         header('Location: index.php?action=users');
